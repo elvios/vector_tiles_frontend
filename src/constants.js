0a76bc63bc9a24152e37b4ccc01fb776176587bb
Elvis <elvisflesborg@protonmail.com>
2024-09-29 08:59:42 +0200

    weave-style added

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const STYLE_FILES = [
   {
     title: 'Klassisk', // The title displayed in the styling selector
     img: 'img/klassisk.png', // File location of the image to be diplayed in the styling selector
-    style: 'styles/official/skaermkort_klassisk.json' // File location of the style file
+    style: 'styles/custom/weave.json' // File location of the style file
   },
   {
     title: 'Dæmpet',
